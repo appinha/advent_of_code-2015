e222cbd6677cc46300cb9b6d0700cb0eb128c9b4
appinha <amanda_pinha@hotmail.com>
2022-11-19 11:30:53 -0300

    [day_02] Updates files to conform to new framework

--- my_solutions/day_02/main.py
+++ my_solutions/day_02/main.py
@@ -1,39 +1,32 @@
-import sys
-sys.path.insert(0, '..')
-from aoc_framework.solver.puzzle_solver import PuzzleSolver
 from pprint import pprint
 
 import itertools as it
 import numpy as np
 
 
-delimiter = "\n"
-
-
-class DayPuzzleSolver(PuzzleSolver):
+class DayPuzzleSolver():
     def __init__(self):
-        self.delimiter = delimiter
-
-    def get_input_into_self(self, raw_input):
-        self.box_dimensions = [self._get_box_dimensions(s) for s in raw_input]
+        self.delimiter = "\n"
 
     def _get_box_dimensions(self, string):
         return list(map(int, string.split('x')))
 
-    def solve_part_1(self):
+    def solve_part_1(self, raw_input):
         areas = []
-        for dimensions in self.box_dimensions:
-            side_dimensions = list(it.combinations(dimensions, 2))
+        for string in raw_input:
+            box_dimensions = self._get_box_dimensions(string)
+            side_dimensions = list(it.combinations(box_dimensions, 2))
             side_areas = [np.prod(dim) for dim in side_dimensions]
             box_area = 2 * sum(side_areas)
             slack = min(side_areas)
             areas.append(box_area + slack)
         return sum(areas)
 
-    def solve_part_2(self):
+    def solve_part_2(self, raw_input):
         lengths = []
-        for dimensions in self.box_dimensions:
-            wrap = 2 * (dimensions[0] + dimensions[1])
-            ribbon = np.prod(dimensions)
+        for string in raw_input:
+            box_dimensions = sorted(self._get_box_dimensions(string))
+            wrap = 2 * (box_dimensions[0] + box_dimensions[1])
+            ribbon = np.prod(box_dimensions)
             lengths.append(wrap + ribbon)
         return sum(lengths)
