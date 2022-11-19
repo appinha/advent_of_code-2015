3f779b6af3f56f5cf6e87c82a0d22e66aec4bdd9
appinha <amanda_pinha@hotmail.com>
2022-11-19 11:25:29 -0300

    [day_01] Updates files to conform to new framework

--- my_solutions/day_01/main.py
+++ my_solutions/day_01/main.py
@@ -1,29 +1,20 @@
-import sys
-sys.path.insert(0, '..')
-from aoc_framework.solver.puzzle_solver import PuzzleSolver
 from pprint import pprint
 
 
-delimiter = ""
-
-
-class DayPuzzleSolver(PuzzleSolver):
+class DayPuzzleSolver():
     def __init__(self):
-        self.delimiter = delimiter
+        self.delimiter = ""
 
-    def get_input_into_self(self, raw_input):
-        self.chars = raw_input
-
-    def solve_part_1(self):
+    def solve_part_1(self, raw_input):
         count = 0
-        for char in self.chars:
+        for char in raw_input:
             count += 1 if char == "(" else -1
         return count
 
-    def solve_part_2(self):
+    def solve_part_2(self, raw_input):
         count = 0
         position = 1
-        for char in self.chars:
+        for char in raw_input:
             count += 1 if char == "(" else -1
             if count == -1:
                 break
