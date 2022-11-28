2b718d016f55710abe787ff89b17a3947837715b
appinha <amanda_pinha@hotmail.com>
2022-11-28 15:07:31 -0300

    [day_02] Adds aoc_lib functions

--- my_solutions/day_02/main.py
+++ my_solutions/day_02/main.py
@@ -2,8 +2,6 @@ import sys; sys.path.insert(0, '..')
 import aoc_lib as lib
 from pprint import pprint
 
-import numpy as np
-
 
 class DayPuzzleSolver():
     def __init__(self):
@@ -17,7 +15,7 @@ class DayPuzzleSolver():
         for string in raw_input:
             box_dimensions = self._get_box_dimensions(string)
             side_dimensions = lib.list_combinations(box_dimensions, 2)
-            side_areas = [np.prod(dim) for dim in side_dimensions]
+            side_areas = [lib.prod(dim) for dim in side_dimensions]
             box_area = 2 * sum(side_areas)
             slack = min(side_areas)
             areas.append(box_area + slack)
@@ -28,6 +26,6 @@ class DayPuzzleSolver():
         for string in raw_input:
             box_dimensions = sorted(self._get_box_dimensions(string))
             wrap = 2 * (box_dimensions[0] + box_dimensions[1])
-            ribbon = np.prod(box_dimensions)
+            ribbon = lib.prod(box_dimensions)
             lengths.append(wrap + ribbon)
         return sum(lengths)
